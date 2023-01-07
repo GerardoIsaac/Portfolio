d7b18c02049c7f728d383a07b49f1baa0cfc6bfb
Gerardo Castañeda <zl1fmercury@gmail.com>
2023-01-07 15:48:43 -0600

    about me section update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
        bg-gradient-to-b from-color2 via-color1 to-color1 text-slate-200"
     >
       <div
-        className="flex flex-col p-8 mt-8 max-w-screen-md 
+        className="flex flex-col p-8 mt-8 max-w-screen-md text-xl 
       sm:mt-0 lg:max-w-4xl xl:max-w-7xl"
       >
         <div className="py-8">
@@ -19,27 +19,27 @@ const About = () => {
             About
           </p>
         </div>
-        <p className="text-xl md:mt-12">
-          Hey there! ✌ I'm a fervent Frontend developer with a strong
-          foundation in HTML, CSS, and JavaScript. I mainly create websites with
-          React. I'm excited to continue learning and growing in my career. With
-          a passion for creating intuitive and visually appealing web
-          applications. Eager to bring my energy and enthusiasm to a dynamic
-          team. In addition to my technical skills, I'm a proactive and
-          detail-oriented learner with solid communication, problem-solving,
-          teamwork, time management, and leadership skills, with ease to adapt
-          well to different environments and push myself to overcome
-          adversities. I'm always looking for ways to improve my skills and stay
-          up to date on the latest technologies and best practices in the field.
+        <p className="md:mt-12">
+          I'm a passionate Frontend developer with a strong foundation in HTML,
+          CSS, and JavaScript. I mainly create websites with React and Tailwind
+          but have worked with technologies like Sass, Bootstrap, and JQuery. I
+          also have basic knowledge of backend technologies like Node.js,
+          Express, and MySQL as databases.
         </p>
         <br />
-        <p className="text-xl">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, cumque
-          perferendis. Distinctio nostrum culpa quis, voluptatibus, officia
-          perferendis asperiores cum provident reiciendis animi obcaecati,
-          libero quos dolor temporibus eligendi rem minus! Aliquam maiores
-          aperiam deleniti sequi impedit ab perferendis numquam est dolores illo
-          optio unde voluptas nesciunt, omnis sint cum.
+        <p>
+          I'm a proactive and detail-oriented learner with solid communication,
+          problem-solving, teamwork, time management, and leadership skills.
+          With ease to adapt well to different environments and push me to
+          overcome adversities.
+        </p>
+        <br />
+        <p>
+          I love music, so I compose and produce songs in my free time or enjoy
+          playing guitar or keys. I take pleasure in a good book with a coffee
+          cup. I like to read about topics that help me to grow, so psychology,
+          personal finances, or interpersonal and intrapersonal skills books are
+          my go-to choices.
         </p>
       </div>
     </section>
