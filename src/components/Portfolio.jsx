c1c725054803577fcc432a89a804971b609afe38
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-23 14:29:33 -0600

    update projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,30 @@
-import bookstore from "../assets/portfolio/bookstore.png";
+import promptopia from "../assets/portfolio/promptopia.png";
 import minecraft from "../assets/portfolio/minecraft.png";
 import blog from "../assets/portfolio/blog.png";
 import movies from "../assets/portfolio/movies.png";
+import hilink from "../assets/portfolio/hilink.png";
 import landingpage from "../assets/portfolio/landingpage.png";
-import sonical from "../assets/portfolio/sonical.png";
+//import sonical from "../assets/portfolio/sonical.png";
 import { FaGithub } from "react-icons/fa";
 import { HiOutlineGlobeAlt } from "react-icons/hi";
 
 const Portfolio = () => {
   const projects = [
     {
-      id: 6,
+      id: 3,
       src: landingpage,
       title: "Mockup landing page",
       tools: "HTML, JavaScript, Tailwind",
       repo: "https://github.com/GerardoIsaac/Landing-page",
       demo: "https://gerardoisaac.github.io/Landing-page/",
+    },
+    {
+      id: 2,
+      src: hilink,
+      title: "UI/UX landing page for a camping app",
+      tools: "Typescript, Next.js, Tailwind",
+      repo: "https://github.com/GerardoIsaac/Camp-Traveler-App",
+      demo: "https://camptraveling.netlify.app/",
     },
     {
       id: 4,
@@ -27,22 +36,20 @@ const Portfolio = () => {
     },
     {
       id: 1,
-      src: bookstore,
-      title: "Bookstore CRUD app",
-      tools: "React, CSS, Node.js, Express, MySQL",
-      repo: "https://github.com/GerardoIsaac/Book-store",
-      demo: "",
+      src: promptopia,
+      title: "Promptopia",
+      tools: "Next.js, Tailwind, MongoDB, Mongoose",
+      repo: "https://github.com/GerardoIsaac/AI-prompts-app",
+      demo: "https://ai-prompts-app-gerardoisaac.vercel.app/",
     },
-
     {
-      id: 3,
+      id: 6,
       src: minecraft,
       title: "Minecraft clone",
       tools: "React, CSS, Three.js",
       repo: "https://github.com/GerardoIsaac/Minecraft-clone",
       demo: "https://gerardoisaac.github.io/Minecraft-clone/",
     },
-
     {
       id: 5,
       src: movies,
@@ -51,14 +58,14 @@ const Portfolio = () => {
       repo: "https://github.com/GerardoIsaac/Movie_search_app",
       demo: "https://gerardoisaac.github.io/Movie_search_app/",
     },
-    {
+    /* {
       id: 7,
       src: sonical,
       title: "Sonical landing page",
       tools: "HTML, Bootstrap, CSS",
       repo: "https://github.com/GerardoIsaac/Sonical-landing-page",
       demo: "https://gerardoisaac.github.io/Sonical-landing-page/",
-    },
+    }, */
   ];
 
   return (
@@ -80,43 +87,45 @@ const Portfolio = () => {
         {/* Cards */}
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {/* Individual card */}
-          {projects.map(({ id, src, title, tools, repo, demo }) => (
-            <div
-              key={id}
-              className="shadow-md shadow-slate-400 rounded-lg group"
-            >
-              <img
-                src={src}
-                alt=""
-                className="rounded-t-md duration-200 hover:scale-105 
+          {projects
+            .sort((a, b) => a.id - b.id)
+            .map(({ id, src, title, tools, repo, demo }) => (
+              <div
+                key={id}
+                className="shadow-md shadow-slate-400 rounded-lg group"
+              >
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-t-md duration-200 hover:scale-105 
               hover:rounded-b-md"
-              />
-              {/* Card info */}
-              <div className="text-center h-[100px] group">
-                {/* Name and techs */}
-                <div className="group-hover:hidden">
-                  <p className="text-lg pt-2">{title}</p>
-                  <div className="w-1/4 border-b-2 border-slate-400 mx-auto pt-1"></div>
-                  <p className="py-4">{tools}</p>
-                </div>
+                />
+                {/* Card info */}
+                <div className="text-center h-[100px] group">
+                  {/* Name and techs */}
+                  <div className="group-hover:hidden">
+                    <p className="text-lg pt-2">{title}</p>
+                    <div className="w-1/4 border-b-2 border-slate-400 mx-auto pt-1"></div>
+                    <p className="py-4">{tools}</p>
+                  </div>
 
-                {/* Live demo and repo buttons */}
-                <div className="hidden h-full group-hover:block text-lg">
-                  <a href={demo} target="_blank" rel="noreferrer">
-                    <button className="w-1/2 mx-auto h-3/5 border-r-2 border-slate-400 hover:text-white">
-                      <HiOutlineGlobeAlt size={30} className='inline-block' /> Watch live
-                    </button>
-                  </a>
-                  <a href={repo} target="_blank" rel="noreferrer">
-                  
-                    <button className="w-1/2 mx-auto h-full hover:text-white">
-                    <FaGithub size={30} className='inline-block' /> Code 
-                    </button>
-                  </a>
+                  {/* Live demo and repo buttons */}
+                  <div className="hidden h-full group-hover:block text-lg">
+                    <a href={demo} target="_blank" rel="noreferrer">
+                      <button className="w-1/2 mx-auto h-3/5 border-r-2 border-slate-400 hover:text-white">
+                        <HiOutlineGlobeAlt size={30} className="inline-block" />{" "}
+                        Watch live
+                      </button>
+                    </a>
+                    <a href={repo} target="_blank" rel="noreferrer">
+                      <button className="w-1/2 mx-auto h-full hover:text-white">
+                        <FaGithub size={30} className="inline-block" /> Code
+                      </button>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </section>
